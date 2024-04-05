05d92b14f8f5895f5aae05ea3f304e35bbacb263
Marcos Gago <mgagog@alumnos.nebrija.es>
2024-04-05 21:14:07 +0200

    practica 3

--- practica-3-main/islands/contact-create-form.tsx
+++ practica-3-main/islands/contact-create-form.tsx
@@ -27,32 +27,27 @@ export default function CreateContactForm() {
   const [isSubmitting, setIsSubmitting] = useState(false);
 
   function validateForm() {
-    let isValid = true;
     const newFormData = { ...formData };
     if (!newFormData.name.value) {
       newFormData.name.error = "Name is required";
-      isValid = false;
     } else {
       newFormData.name.error = "";
     }
 
     if (!newFormData.lastName.value) {
       newFormData.lastName.error = "Last Name is required";
-      isValid = false;
     } else {
       newFormData.lastName.error = "";
     }
 
     if (!newFormData.email.value) {
       newFormData.email.error = "Email is required";
-      isValid = false;
     } else {
         newFormData.email.error = "";
     }
 
     if (!newFormData.gender.value) {
       newFormData.gender.error = "Gender is required";
-      isValid = false;
     } else {
       newFormData.gender.error = "";
     }
