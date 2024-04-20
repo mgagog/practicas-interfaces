e1bd13a81ff2edc8c7a05c72e8193ac126281c18
Marcos Gago <mgagog@alumnos.nebrija.es>
2024-04-20 04:46:33 +0200

    practica 4

--- practica4/islands/Pokemon.tsx
+++ practica4/islands/Pokemon.tsx
@@ -1,41 +1,59 @@
 import { useEffect, useState } from "preact/hooks";
 import { FunctionComponent } from "preact";
-import { Pokemon } from "../types.ts"
+import { Pokemon } from "../types.ts";
 
 const Pokemons: FunctionComponent = () => {
   const [page, setPage] = useState<number>(1);
   const [query, setQuery] = useState<string>("");
   const [pokemon, setPokemon] = useState<Pokemon[]>([]);
 
-  useEffect(() => { 
-    fetch(`https://fernandomur-random-data-72.deno.dev/pokemon?page=${page}&query=${query}`)
-  .then((response) => response.json())
-  .then((data) => {
-    setPokemon(data);
-  });
-}, [page, query]);
-  
+  useEffect(() => {
+    fetch(
+      `https://fernandomur-random-data-72.deno.dev/pokemon?page=${page}&query=${query}`,
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setPokemon(data);
+      });
+  }, [page, query]);
+
   return (
     <div class="island pokemon">
-            <input placeholder={"Buscar"} onInput={(e) => { setQuery(e.currentTarget.value); setPage(1)}}/>
-            <div class="info pokemon">
-            {pokemon.length === 0 && <h2 class="error">Sin datos</h2>}
-                {pokemon.map(p => {
-                    return (
-                    <div class="elem">
-                        <p>ID: {p.id}</p>
-                        <p>Name: {p.name}</p>
-                        <p>Type: <b class={"type " + p.type}>{p.type}</b></p>
-                        <p>Base experience: {p.base_experience}</p>
-                    </div>
-                    )
-                })}
+      <input
+        placeholder={"Buscar"}
+        onInput={(e) => {
+          setQuery(e.currentTarget.value);
+          setPage(1);
+        }}
+      />
+      <div class="info pokemon">
+        {pokemon.length === 0 && <h2 class="error">Sin datos</h2>}
+        {pokemon.map((p) => {
+          return (
+            <div class="elem">
+              <p>ID: {p.id}</p>
+              <p>Name: {p.name}</p>
+              <p>
+                Type: <b class={"type " + p.type}>{p.type}</b>
+              </p>
+              <p>Base experience: {p.base_experience}</p>
             </div>
-            <div class="buttons">
-                {page > 1 && <button onClick={() => setPage(page - 1)}>Anterior</button>}
-                {page < 13 && <button disabled={pokemon.length === 0} onClick={() => setPage(page + 1)}>Siguiente</button>}
-            </div>
-        </div>
+          );
+        })}
+      </div>
+      <div class="buttons">
+        {page > 1 && <button onClick={() => setPage(page - 1)}>Anterior
+        </button>}
+        {page < 13 && (
+          <button
+            disabled={pokemon.length === 0}
+            onClick={() => setPage(page + 1)}
+          >
+            Siguiente
+          </button>
+        )}
+      </div>
+    </div>
   );
 };
 
--- practica4/islands/Quotes.tsx
+++ practica4/islands/Quotes.tsx
@@ -3,36 +3,52 @@ import { FunctionComponent } from "preact";
 import { Quote } from "../types.ts";
 
 const Quotes: FunctionComponent = () => {
-    const [page, setPage] = useState<number>(1);
+  const [page, setPage] = useState<number>(1);
   const [query, setQuery] = useState<string>("");
   const [quotes, setQuotes] = useState<Quote[]>([]);
 
-  useEffect(() => { 
-    fetch(`https://fernandomur-random-data-72.deno.dev/quotes?page=${page}&query=${query}`)
-  .then((response) => response.json())
-  .then((data) => {
-    setQuotes(data);
-  });
-}, [page, query]);
+  useEffect(() => {
+    fetch(
+      `https://fernandomur-random-data-72.deno.dev/quotes?page=${page}&query=${query}`,
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        setQuotes(data);
+      });
+  }, [page, query]);
   return (
     <div class="island quotes">
-            <input placeholder={"Buscar"} onInput={(e) => { setQuery(e.currentTarget.value); setPage(1)}}/>
-            <div class="info">
-                {quotes.length === 0 && <h2 class="error">Sin datos</h2>}
-                {quotes.map(q => {
-                    return (
-                    <div class="elem" key={q.id}>
-                        <p>ID: {q.id}</p>
-                        <p>Quote: {q.quote}</p>
-                    </div>
-                    )
-                })}
+      <input
+        placeholder={"Buscar"}
+        onInput={(e) => {
+          setQuery(e.currentTarget.value);
+          setPage(1);
+        }}
+      />
+      <div class="info">
+        {quotes.length === 0 && <h2 class="error">Sin datos</h2>}
+        {quotes.map((q) => {
+          return (
+            <div class="elem" key={q.id}>
+              <p>ID: {q.id}</p>
+              <p>Quote: {q.quote}</p>
             </div>
-            <div class="buttons">
-                {page > 1 && <button onClick={() => setPage(page - 1)}>Anterior</button>}
-                {page < 10 && <button disabled={quotes.length === 0} onClick={() => setPage(page + 1)}>Siguiente</button>}
-            </div>
-        </div>
+          );
+        })}
+      </div>
+      <div class="buttons">
+        {page > 1 && <button onClick={() => setPage(page - 1)}>Anterior
+        </button>}
+        {page < 10 && (
+          <button
+            disabled={quotes.length === 0}
+            onClick={() => setPage(page + 1)}
+          >
+            Siguiente
+          </button>
+        )}
+      </div>
+    </div>
   );
 };
 
